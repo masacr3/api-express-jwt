fc6bd1998af9b87088752f1339e2029681ab92b4
masacr3 <leorhcp2030@gmail.com>
2023-02-26 02:34:37 -0300

    agrego bodyparser

--- index.js
+++ index.js
@@ -5,21 +5,31 @@ const jwt = require("jsonwebtoken")
 
 const port = process.env.PORT || 3333
 const secret = process.env.SECRET 
+const invitado = process.env.USER //invitado
+const passInvitado = process.env.USERPASS //1234
+
+const admin = process.env.ADMINUSER // ahre
+const passAdmin = process.env.ADMINPASS //doble ahre
 
 const app = express()
 
 app.use( cors())
 
+app.use( express.json())
+
 app.post("/token", (req, res) => {
+    const user = req.body.email
+    const {sub, name} = { sub : user !== admin ? "invitado" : admin, name : user !== admin ? invitado : admin}
+
     //Get user from DB
-    const { id: sub, name} = { id : "masacr3", name :"leo"} 
     const token = jwt.sign({
         sub,
         name,
         exp: Date.now() + 60 * 1000
     }, secret)
 
-    res.send( {token })
+    res.send( {token, role:name})
+    
 })
 
 app.get("/public", (req, res) =>{
